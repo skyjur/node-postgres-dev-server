79b2bf520b948ae59ac5b129e046606d8058a938
Skirmantas Jurgaitis <skyjur@gmail.com>
2018-03-29 12:53:01 +0800

    Fix download script

--- download.ts
+++ download.ts
@@ -1,30 +1,31 @@
-const os = require('os');
-const http = require('https');
-const fs = require('fs');
-const path = require('path');
-const child_process = require('child_process');
-const process = require('process');
-const ProgressBar = require('progress');
+import * as os from 'os';
+import * as http from 'https';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as child_process from 'child_process';
+import * as process from 'process';
+import ProgressBar from 'progress';
 
 const DEFAULT_DOWNLOAD_URLS = {
     linux: 'https://raw.githubusercontent.com/skyjur/node-postgres-dev-server/master/binaries/postgresql-10.3-1-linux-x64-binaries.tar.gz',
     win32: 'https://raw.githubusercontent.com/skyjur/node-postgres-dev-server/master/binaries/postgresql-10.3-1-windows-x64-binaries.zip',
-    darwin: 'https://raw.githubusercontent.com/skyjur/node-postgres-dev-server/master/binaries/postgresql-10.3-1-osx-binaries.zip',
-    linux: 'https://raw.githubusercontent.com/skyjur/node-postgres-dev-server/master/binaries/postgresql-10.3-1-windows-x64-binaries.zip'
+    darwin: 'https://raw.githubusercontent.com/skyjur/node-postgres-dev-server/master/binaries/postgresql-10.3-1-osx-binaries.zip'
 };
 
 
-function getPostgresql(location, dowload_urls) {
-    var dowload_urls = dowload_urls || DEFAULT_DOWNLOAD_URLS;
-    var url = dowload_urls[os.platform()];
-    if(!url) {
+function getPostgresql(location: string, user_defined_dowload_urls?: Partial<typeof DEFAULT_DOWNLOAD_URLS>) : Promise<any> {
+    var download_urls = {... DEFAULT_DOWNLOAD_URLS, ... (user_defined_dowload_urls || {})};
+    var platform = process.env.PLATFORM || os.platform();
+    if(platform in download_urls) {
+        var url = download_urls[platform];
+        var localPath = path.join(location, path.basename(url));
+        return downloadFile(url, localPath)
+            .then(() => console.log('Extracting postgresql...'))
+            .then(() => extract(localPath, location))
+            .catch(console.error);
+    } else {
         throw new Error(`Have no download link for platform: ${os.platform()}`)
     }
-    var localPath = path.join(location, path.basename(url));
-    return downloadFile(url, localPath)
-        .then(() => console.log('Extracting postgresql...'))
-        .then(() => extract(localPath, location))
-        .catch(console.error);
 }
 
 
@@ -42,16 +43,17 @@ function downloadFile(url, localPath) {
                     complete: '=',
                     incomplete: ' ',
                     width: 20,
-                    total: parseInt(response.headers['content-length'], 10)
+                    total: parseInt(response.headers['content-length']!, 10)
                   });
                 response.on('data', chunk => bar.tick(chunk.length));
                 response.on('error', (err) => {
-                    unlink(localPath);
+                    fs.unlink(localPath, () => null);
                     reject(err);
                 });
                 response.pipe(file);
                 file.on('finish', () => {
-                    file.close(resolve);
+                    file.end();
+                    resolve();
                 });
             }
         });
@@ -73,17 +75,19 @@ function call(command, args) {
     })
 }
 
-function extract(filepath, location) {
-    if(1 || os.platform() == 'win32') {
+function extract(filepath: string, location: string) {
+    if(os.platform() == 'win32') {
         return call('PowerShell', ['-Command', `Expand-Archive -Path "${filepath}" -DestinationPath "${location}"`]);
     } else if(/tar.gz$/.test(filepath)) {
         return call('tar', ['-x', '-f', filepath, '-C', location, '--overwrite']);
     } else if(/zip$/.test(filepath)) {
         return call('unzip', ['-qod', location, filepath]);
+    } else {
+        throw new Error('No rule to extract file');
     }
 }
 
-function ensureHaveBinaries(location) {
+function ensureHaveBinaries(location: string) {
     if(fs.existsSync(path.join(location, 'pgsql', 'bin'))) {
         return Promise.resolve();
     } else {
